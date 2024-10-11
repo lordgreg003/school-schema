67e3e5b34b298d402b56eb8efc9f11be18693ca6
chidera nwokoye <chideranwokoye555@gmail.com>
2024-10-11 12:01:17 +0100

    student[screen]

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,7 +1,23 @@
+import Announcement from "@/components/Announcement";
+import EventCalender from "@/components/EventCalender";
 import React from "react";
 
 const page = () => {
-  return <div>am student</div>;
+  return (
+    <div className="p-4 flex flex-col xl:flex-row">
+      {/* left */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1>schedule (4A)</h1>
+        </div>
+      </div>
+      {/* right */}
+      <div className="w-full xl:w-1/2 flex  flex-col gap-8">
+        <EventCalender />
+        <Announcement />
+      </div>
+    </div>
+  );
 };
 
 export default page;
